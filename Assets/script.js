614abe96ccc2bdd092a810c237119c1c624aa91d
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-01-18 16:57:51 -0500

    cleaned up folders/file titles, cleaned up html and css files

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/*
+1. output password that meets user criteria
+2. criteria
+  a. length (beetween 8 - 128)
+  b. ask to include:
+      lowercase
+      uppercase
+      numeric
+      special characters
+    input should be validated and have at least one of each chosen type
+3. either displayed in an alert or written to the page
+*/
