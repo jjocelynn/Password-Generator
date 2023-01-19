09be77b4f6ea0f89597ea2333906ab07105ee740
Jocelyn <115833657+jjocelynn@users.noreply.github.com>
2023-01-18 23:45:00 -0500

    worked on js(added window prompts for length, lowercase, uppercase, number, and special character when the generate button is pressed

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,32 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+generateBtn.addEventListener("click", writePassword);
 function writePassword() {
+
+  //asking the user for a password length
+  let lengthInput = prompt("Choose a password length");
+
+  while (lengthInput < 8 || lengthInput > 128) {
+    lengthInput = prompt("*Must be a number between 8 and 128 characters");
+  }
+
+  console.log("The user chose a length of: " + lengthInput);
+
+  //asking the user if they would like lowercase letters
+  let lowercase = confirm("would you like to include lowercase letters? (enter 'ok' for yes and 'cancel' for no)");
+  console.log("The user wants lowercase: " + lowercase);
+
+  let uppercase = confirm("would you like to include uppercase letters? (enter 'ok' for yes and 'cancel' for no)");
+  console.log("The user wants uppercase: " + uppercase);
+
+  let numbers = confirm("would you like to include numbers? (enter 'ok' for yes and 'cancel' for no)");
+  console.log("The user wants numbers: " + numbers);
+
+  let sChar = confirm("would you like to include special characters? (enter 'ok' for yes and 'cancel' for no)");
+  console.log("The user wants special characters: " + sChar);
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,7 +36,10 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
 
 /*
 1. output password that meets user criteria
